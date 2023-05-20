57d22b73aee3323cfea6c2f17d19fcc67165a6b3
Naschfuchs1 <134136625+Naschfuchs1@users.noreply.github.com>
2023-05-20 15:12:21 +0200

    Update urlcombi.py

--- urlcombi.py
+++ urlcombi.py
@@ -1,18 +1,21 @@
 import itertools
 
 words = [
-    "Washed", "Frost", "Robot", "Misfits", "Shady", "Goat", "Rocked", "Skull", "Lives",
-    "Handy", "Robot", "Chest", "Dawn", "Turkey", "Diet", "Parrot", "Chickens", "Locker",
-    "Cannonball"
+    "Washed", "Frost", "Robot", "Misfits", "Shady", "Goat", "Rocked",
+    "Skull", "Lives", "Handy", "Robot", "Chest", "Dawn", "Turkey", "Diet",
+    "Parrot", "Chickens", "Locker", "Cannonball"
 ]
 
-combinations = itertools.combinations(words, 3)
+# Generate all permutations of 3-word combinations
+combinations = itertools.permutations(words, 3)
 
-output_path = r"X:\Users\XXX\Desktop\123.txt"
+# File path to store the results
+output_file = "X:\\Users\\XXX\\Desktop\\combinationurls.txt"
 
-with open(output_path, "w") as file:
+# Iterate over the combinations and write them to the output file
+with open(output_file, "w") as file:
     for combination in combinations:
-        if len(set(combination)) == 3:
-            file.write("https://what3words.com/" + ".".join(combination) + "\n")
+        url = "https://what3words.com/" + ".".join(combination) + "\n"
+        file.write(url)
 
-print(f"File created at: {output_path}")
+print("Permutations written to", output_file)
